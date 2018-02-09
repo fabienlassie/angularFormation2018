1cc7058ca53a8920a09130d1961d3cc987c55a15
fabienLassie <fabien.lassie@gmail.com>
2018-02-09 09:52:50 +0100

    commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { ProductComponent } from './product/product.component';
+import { PricePipe } from './price.pipe';
+import { CategoryPipe } from './category.pipe';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductService } from './product.service';
+import { CartService } from './cart.service';
+import { ProductCartComponent } from './product-cart/product-cart.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductGuard } from './product.guard';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogComponent,
+    ProductComponent,
+    PricePipe,
+    CategoryPipe,
+    HomeComponent,
+    ContactComponent,
+    CartComponent,
+    ProductCartComponent,
+    ProductDetailComponent
   ],
   imports: [
-    BrowserModule
+    HttpModule,
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([{
+      path: "catalog", component: CatalogComponent, pathMatch: "full"
+    }, {
+      path: "", component: HomeComponent, pathMatch: "full"
+    }, {
+      path: "home", component: HomeComponent, pathMatch: "full"
+    }, {
+      path: "contact", component: ContactComponent, pathMatch: "full"
+    }, {
+      path: "cart", component: CartComponent, pathMatch: "full"
+    }, {
+      path: "detail/:ref", component: ProductDetailComponent, pathMatch: "full", "canActivate":[ProductGuard]
+    }], {useHash: true})
   ],
-  providers: [],
+  providers: [ProductService, CartService, ProductGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
